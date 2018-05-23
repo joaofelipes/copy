e3f8ff242627106bed0924cf8b7bd7d555875066
joaofelipes <joaofelipes@gmail.com>
2018-05-23 11:37:02 -0300

    18052311-113701

--- js/script.js
+++ js/script.js
@@ -6,24 +6,56 @@ function submitMessage() {
     })
     .then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
+        notify("Text was save!");
     })
     .catch(function(error) {
         console.error("Error adding document: ", error);
+        notify("Error saving text.")
     });
  }
 
 function getMessage() {
     var resultData;
-    db.collection("TextPieces").orderBy("date", "desc").limit(1).get().then(function(results) {
-        results.forEach(function(doc) {
-            $("#message").val(doc.data().text);
-        });
-    });;
+    try {
+        db.collection("TextPieces").orderBy("date", "desc").limit(1).get().then(function(results) {
+            results.forEach(function(doc) {
+                $("#message").val(doc.data().text);
+            });
+        });;
+        notify("Text was recovered.");
+    }
+    catch {
+        notify("Error fetching data.");
+    };
 }
 
+function copyMessage() {
+    var copyText = document.getElementById("message");
+    copyText.select();
+    document.execCommand("copy");
+    notify("Text was sent do clipboard.")
+}
+
+
+function notify (message) {
+    var notification = document.querySelector('.mdl-js-snackbar');
+    notification.MaterialSnackbar.showSnackbar(
+      {
+        message: message,
+        timeout: 2000,
+        actionHandler: function(event) {},
+        actionText: 'Done'
+      }
+    );    
+
+}
+
+function dummie (){}
+
 $( document ).ready(function(){
-    $( ".btnok" ).on( "click", submitMessage );
-    $( ".btnget" ).on( "click", getMessage );
+    $( "#btnsave" ).on( "click", submitMessage );
+    $( "#btnget" ).on( "click", getMessage );
+    $( "#btncopy" ).on( "click", copyMessage );
 });
 
 
